d9637ad686c80c13fe60eecb4ced2ee1ea026940
donus1perdonus <ekzozeta@gmail.com>
2024-10-07 17:23:33 +0300

    final allocator_global_heap

--- allocator/allocator_global_heap/src/allocator_global_heap.cpp
+++ allocator/allocator_global_heap/src/allocator_global_heap.cpp
@@ -1,49 +1,109 @@
-#include <not_implemented.h>
-
 #include "../include/allocator_global_heap.h"
 
 allocator_global_heap::allocator_global_heap(
-    logger *logger)
+    logger *logger) 
+    : _logger(logger)
 {
-    throw not_implemented("allocator_global_heap::allocator_global_heap(logger *)", "your code should be here...");
+    log_with_guard(get_typename() + 
+        " allocator_global_heap::allocator_global_heap(logger *logger)",
+        logger::severity::debug);
 }
 
 allocator_global_heap::~allocator_global_heap()
 {
-    throw not_implemented("allocator_global_heap::~allocator_global_heap()", "your code should be here...");
+    log_with_guard(get_typename() + 
+        " allocator_global_heap::~allocator_global_heap()",
+        logger::severity::debug);
+
+    delete _logger;
 }
 
 allocator_global_heap::allocator_global_heap(
     allocator_global_heap &&other) noexcept
+    : _logger(std::move(other._logger))
 {
-    throw not_implemented("allocator_global_heap::allocator_global_heap(allocator_global_heap &&) noexcept", "your code should be here...");
+    log_with_guard(get_typename() + 
+        " allocator_global_heap::allocator_global_heap(allocator_global_heap &&other) noexcept",
+        logger::severity::debug);
+
+    other._logger = nullptr;
 }
 
 allocator_global_heap &allocator_global_heap::operator=(
     allocator_global_heap &&other) noexcept
 {
-    throw not_implemented("allocator_global_heap &allocator_global_heap::operator=(allocator_global_heap &&) noexcept", "your code should be here...");
+    log_with_guard(get_typename() + 
+        " allocator_global_heap &allocator_global_heap::operator=(allocator_global_heap &&other) noexcept",
+        logger::severity::debug);
+
+    if (this != &other)
+    {
+        _logger = std::move(other._logger);
+        other._logger = nullptr;
+    }
+
+    return *this;
 }
 
 [[nodiscard]] void *allocator_global_heap::allocate(
     size_t value_size,
     size_t values_count)
 {
-    throw not_implemented("[[nodiscard]] void *allocator_global_heap::allocate(size_t, size_t)", "your code should be here...");
+    log_with_guard(get_typename() + 
+        " [[nodiscard]] void *allocator_global_heap::allocate()",
+        logger::severity::debug);
+
+    void* memory = nullptr;
+
+    try 
+    {
+        memory = ::operator new(value_size * values_count);
+        
+        log_with_guard(get_typename() + 
+            " Memory allocated succesfully", 
+            logger::severity::debug);
+    
+    } catch (const std::bad_alloc& e) {
+        
+        log_with_guard(get_typename() + 
+            " Memory allocate error",
+            logger::severity::debug);
+        
+        throw;
+    }
+
+    return memory;
 }
 
 void allocator_global_heap::deallocate(
     void *at)
 {
-    throw not_implemented("void allocator_global_heap::deallocate(void *)", "your code should be here...");
+    log_with_guard(get_typename() + 
+        " void allocator_global_heap::deallocate()",
+        logger::severity::debug);
+
+    if (at == nullptr) 
+    {
+        log_with_guard(get_typename() + 
+            "Attempt to deallocate null pointer",
+            logger::severity::debug);
+
+        throw std::logic_error("Attempt to deallocate null pointer");
+    }
+    
+    log_with_guard(get_typename() + 
+        " Deallocate succesfully", 
+        logger::severity::debug);
+
+    ::operator delete(at);
 }
 
 inline logger *allocator_global_heap::get_logger() const
 {
-    throw not_implemented("inline logger *allocator_global_heap::get_logger() const", "your code should be here...");
+    return _logger;
 }
 
 inline std::string allocator_global_heap::get_typename() const noexcept
 {
-    throw not_implemented("inline std::string allocator_global_heap::get_typename() const noexcept", "your code should be here...");
+    return "[allocator_global_heap]";
 }
